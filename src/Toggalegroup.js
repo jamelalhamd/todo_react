a58a5d90683339e5527aea1aa2ddbe4f1bb4b35f
Jamel <j_hamad83@hotmail.com>
2024-05-04 23:45:55 +0200

    add completed und not completed button logig

--- src/Toggalegroup.js
+++ src/Toggalegroup.js
@@ -2,11 +2,32 @@ import * as React from 'react';
 import './App.css'
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-
+import { todocontext } from './Context/Todocontext';
 export default function Togglegroup() {
+  const {todosarray,settodoarry}=React.useContext(todocontext);
+  const {fullarry,setfullatty} = React.useState(todosarray);
+  const todos = JSON.parse(localStorage.getItem('todosarray'));
+ function alltask ()
+ {
 
 
+  settodoarry(todos);
+  
 
+
+ }
+
+ function completetask ()
+ {
+
+  const completed = todos.filter((t) => t.iscompleted === true);
+  settodoarry(completed);
+
+ }
+ function notcompletedtask() {
+  const notcompleted = todos.filter((t) => t.iscompleted === false);
+  settodoarry(notcompleted);
+}
 
   return (
     <ToggleButtonGroup
@@ -15,13 +36,16 @@ export default function Togglegroup() {
 
       aria-label="text alignment"
     >
-      <ToggleButton  className='toggle'   value="left" aria-label="left aligned">
+      <ToggleButton 
+      
+    onClick={completetask}
+      className='toggle'   value="left" aria-label="left aligned">
    Done
       </ToggleButton>
-      <ToggleButton className='toggle'value="center" aria-label="centered">
-    Edit
+      <ToggleButton onClick={notcompletedtask} className='toggle'value="center" aria-label="centered">
+   not Done
       </ToggleButton>
-      <ToggleButton className='toggle' value="right" aria-label="right aligned">
+      <ToggleButton  onClick={alltask} className='toggle' value="right" aria-label="right aligned">
 
 All
       </ToggleButton>
